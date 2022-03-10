cd1cce9b1de4e1205c0bcd490caedb075229aa1b
Preethi KCS <99183346+Preethi-Sundaravelu@users.noreply.github.com>
2022-03-10 15:07:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+//Your first program . Here hello world is also data
+console.log("Data Types")
+//Different data types
+var nam="This is example of string data type"
+var num=10
+var num2=30
+console.log("Performing operations on data types")
+var num3=num*num2
+console.log(num3)
+console.log(nam)
+//float
+var pi=3.14
+console.log(pi)
+//Scope blocked in LET
+let r=10
+console.log(r)
+//Circumference: 2*pi*r
+const pir=2*r*pi
+var peri=Math.ceil(pir)
+var cir="Circle"
+console.log("Perimeter of "+cir+" is : "+peri)
+let area=pi*r*r
+console.log("Area of "+cir+" is : "+area)
